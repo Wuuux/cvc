0980be2079e18be1e5c071ed606a5c911e1273f6
Wojciech Wiński <wojciech.winski@gmail.com>
2017-02-22 21:59:03 +0100

    wyszukiwanie 5 kółek lub krzyzyków

--- js/app.js
+++ js/app.js
@@ -7,14 +7,15 @@ $(document).ready(function(){
       window.location.replace("https://github.com/Wuuux/cvc");
     };
   });
-  var time = 0;
-  var wait_5s = setInterval(function(){ myTimer() }, 1000);
-
-  function myTimer() {
-      time++;
-      if (time == 5) time = 0;
-      return time;
-  }
+  // var time = 0;
+  // var wait_5s = setInterval(function(){ myTimer() }, 1000);
+  //
+  // function myTimer() {
+  //     time++;
+  //     if (time == 5) time = 0;
+  //     console.log(time);
+  //     return time;
+  // }
 
   var player = 'circle';
   $('#player').on('click', function(){
@@ -34,6 +35,10 @@ $(document).ready(function(){
       this.$board;
       this.crossFlag = false;
       this.circleFlag = true;
+      this.circleArray = [];
+      this.arrayOfEmptySpaceAroundCircles = [];
+      this.crossArray = [];
+      this.arrayOfEmptySpaceAroundCrosses = [];
 
       this.init = function( _size ){
         this.size = _size;
@@ -49,13 +54,169 @@ $(document).ready(function(){
         console.log(this.board);
       };
 
+      this.findXstringOfValues = function( _x, _y, _direction, length, value ){
+        if (value == 'circle')  v = 1
+        else                    v = -1;
+        switch(true) {
+              case (_direction == 'NW'):
+                  if (this.board[ _x-1 ][ _y-1 ][0] == v) {
+                    if (length-1 == 1) return true;
+                    return this.findXstringOfValues(_x-1, _y-1, 'NW', length-1, value);
+                  };
+                  return false;
+              break;
+              case (_direction == 'NN'):
+                  if (this.board[ _x ][ _y-1 ][0] == v) {
+                    if (length-1 == 1) return true;
+                    return this.findXstringOfValues(_x, _y-1, 'NN', length-1, value);
+                  };
+                  return false;
+              break;
+              case (_direction == 'NE'):
+                  if (this.board[ _x+1 ][ _y-1 ][0] == v) {
+                    if (length-1 == 1) return true;
+                    return this.findXstringOfValues(_x+1, _y-1, 'NE', length-1, value);
+                  };
+                  return false;
+              break;
+              case (_direction == 'EE'):
+                  if (this.board[ _x+1 ][ _y ][0] == v) {
+                    if (length-1 == 1) return true;
+                    return this.findXstringOfValues(_x+1, _y, 'EE', length-1, value);
+                  };
+                  return false;
+              break;
+              case (_direction == 'SE'):
+                  if (this.board[ _x+1 ][ _y+1 ][0] == v) {
+                    if (length-1 == 1) return true;
+                    return this.findXstringOfValues(_x+1, _y+1, 'SE', length-1, value);
+                  };
+                  return false;
+              break;
+              case (_direction == 'SS'):
+                  if (this.board[ _x ][ _y+1 ][0] == v) {
+                    if (length-1 == 1) return true;
+                    return this.findXstringOfValues(_x, _y+1, 'SS', length-1, value);
+                  };
+                  return false;
+              break;
+              case (_direction == 'SW'):
+                  if (this.board[ _x-1 ][ _y+1 ][0] == v) {
+                    if (length-1 == 1) return true;
+                    return this.findXstringOfValues(_x-1, _y+1, 'SW', length-1, value);
+                  };
+                  return false;
+              break;
+              case (_direction == 'WW'):
+                  if (this.board[ _x-1 ][ _y ][0] == v) {
+                    if (length-1 == 1) return true;
+                    return this.findXstringOfValues(_x-1, _y, 'WW', length-1, value);
+                  };
+                  return false;
+              break;
+        };
+
+      };
+
+      this.findFiveCircles = function(){
+        for (var i = 0; i < this.circleArray.length; i++) {
+          if (this.findXstringOfValues(this.circleArray[i][0],this.circleArray[i][1],'NW',5,'circle') == true) return true;
+          if (this.findXstringOfValues(this.circleArray[i][0],this.circleArray[i][1],'NN',5,'circle') == true) return true;
+          if (this.findXstringOfValues(this.circleArray[i][0],this.circleArray[i][1],'NE',5,'circle') == true) return true;
+          if (this.findXstringOfValues(this.circleArray[i][0],this.circleArray[i][1],'EE',5,'circle') == true) return true;
+          if (this.findXstringOfValues(this.circleArray[i][0],this.circleArray[i][1],'SE',5,'circle') == true) return true;
+          if (this.findXstringOfValues(this.circleArray[i][0],this.circleArray[i][1],'SS',5,'circle') == true) return true;
+          if (this.findXstringOfValues(this.circleArray[i][0],this.circleArray[i][1],'SW',5,'circle') == true) return true;
+          if (this.findXstringOfValues(this.circleArray[i][0],this.circleArray[i][1],'WW',5,'circle') == true) return true;
+        };
+        return false;
+      };
+
+      this.findFiveCrosses = function(){
+        for (var i = 0; i < this.crossArray.length; i++) {
+          if (this.findXstringOfValues(this.crossArray[i][0],this.crossArray[i][1],'NW',5,'cross') == true) return true;
+          if (this.findXstringOfValues(this.crossArray[i][0],this.crossArray[i][1],'NN',5,'cross') == true) return true;
+          if (this.findXstringOfValues(this.crossArray[i][0],this.crossArray[i][1],'NE',5,'cross') == true) return true;
+          if (this.findXstringOfValues(this.crossArray[i][0],this.crossArray[i][1],'EE',5,'cross') == true) return true;
+          if (this.findXstringOfValues(this.crossArray[i][0],this.crossArray[i][1],'SE',5,'cross') == true) return true;
+          if (this.findXstringOfValues(this.crossArray[i][0],this.crossArray[i][1],'SS',5,'cross') == true) return true;
+          if (this.findXstringOfValues(this.crossArray[i][0],this.crossArray[i][1],'SW',5,'cross') == true) return true;
+          if (this.findXstringOfValues(this.crossArray[i][0],this.crossArray[i][1],'WW',5,'cross') == true) return true;
+        };
+        return false;
+      };
+
+      this.addToCircleArray = function(_x,_y){
+       this.circleArray.push([_x,_y]);
+      };
+      this.addToCrossArray = function(_x,_y){
+       this.crossArray.push([_x,_y]);
+      };
+
+      function arrInArr(arr, innerArr){
+        var flag = 0;
+        for (var i = 0; i < arr.length; i++) {
+          flag = 1;
+          for (var j = 0; j < innerArr.length; j++) {
+            if (arr[i][j] != innerArr[j]) flag = 0;
+          };
+          if (flag == 1) return 1;
+        };
+        return -1;
+      };
+
+      this.setArrayOfEmptySpaceAroundCircles = function(){
+        console.log(this.circleArray);
+        this.arrayOfEmptySpaceAroundCircles = [];
+        for (var i = 0; i < this.circleArray.length; i++) {
+          var x = this.circleArray[i][0];
+          var y = this.circleArray[i][1];
+          console.log(x,y);
+          console.log(this.board[x-1][y-1]);
+          console.log(this.arrayOfEmptySpaceAroundCircles.indexOf([x-1,y-1]));
+
+          if ((x>4) && (y>4) && (this.board[x-1][y-1][0] == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCircles,[x-1,y-1]) < 0)) this.arrayOfEmptySpaceAroundCircles.push([x-1,y-1]);
+          if ((y>4) &&(this.board[x  ][y-1][0]   == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCircles,[x,y-1])   < 0)) this.arrayOfEmptySpaceAroundCircles.push([x,y-1]);
+          if ((x<this.size-4) && (y>4) &&(this.board[x+1][y-1][0] == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCircles,[x+1,y-1]) < 0)) this.arrayOfEmptySpaceAroundCircles.push([x+1,y-1]);
+          if ((x<this.size-4) &&(this.board[x+1][y  ][0]   == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCircles,[x+1,y])   < 0)) this.arrayOfEmptySpaceAroundCircles.push([x+1,y]);
+          if ((x<this.size-4) && (y<this.size-4) &&(this.board[x+1][y+1][0] == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCircles,[x+1,y+1]) < 0)) this.arrayOfEmptySpaceAroundCircles.push([x+1,y+1]);
+          if ((y<this.size-4) &&(this.board[x  ][y+1][0]   == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCircles,[x,y+1])   < 0)) this.arrayOfEmptySpaceAroundCircles.push([x,y+1]);
+          if ((x>4) && (y<this.size-4) &&(this.board[x-1][y+1][0] == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCircles,[x-1,y+1]) < 0)) this.arrayOfEmptySpaceAroundCircles.push([x-1,y+1]);
+          if ((x>4) &&(this.board[x-1][y  ][0]   == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCircles,[x-1,y])   < 0)) this.arrayOfEmptySpaceAroundCircles.push([x-1,y]);
+
+        };
+        console.log('arrayOfEmptySpaceAroundCircles:', this.arrayOfEmptySpaceAroundCircles);
+      };
+
+      this.setArrayOfEmptySpaceAroundCrosses = function(){
+
+        this.arrayOfEmptySpaceAroundCrosses = [];
+        for (var i = 0; i < this.crossArray.length; i++) {
+          var x = this.crossArray[i][0];
+          var y = this.crossArray[i][1];
+
+          if ((x>4) && (y>4) && (this.board[x-1][y-1][0] == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCrosses,[x-1,y-1]) < 0)) this.arrayOfEmptySpaceAroundCrosses.push([x-1,y-1]);
+          if ((y>4) &&(this.board[x  ][y-1][0]   == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCrosses,[x,y-1])   < 0)) this.arrayOfEmptySpaceAroundCrosses.push([x,y-1]);
+          if ((x<this.size-4) && (y>4) &&(this.board[x+1][y-1][0] == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCrosses,[x+1,y-1]) < 0)) this.arrayOfEmptySpaceAroundCrosses.push([x+1,y-1]);
+          if ((x<this.size-4) &&(this.board[x+1][y  ][0]   == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCrosses,[x+1,y])   < 0)) this.arrayOfEmptySpaceAroundCrosses.push([x+1,y]);
+          if ((x<this.size-4) && (y<this.size-4) &&(this.board[x+1][y+1][0] == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCrosses,[x+1,y+1]) < 0)) this.arrayOfEmptySpaceAroundCrosses.push([x+1,y+1]);
+          if ((y<this.size-4) &&(this.board[x  ][y+1][0]   == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCrosses,[x,y+1])   < 0)) this.arrayOfEmptySpaceAroundCrosses.push([x,y+1]);
+          if ((x>4) && (y<this.size-4) &&(this.board[x-1][y+1][0] == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCrosses,[x-1,y+1]) < 0)) this.arrayOfEmptySpaceAroundCrosses.push([x-1,y+1]);
+          if ((x>4) &&(this.board[x-1][y  ][0]   == 0) && (arrInArr(this.arrayOfEmptySpaceAroundCrosses,[x-1,y])   < 0)) this.arrayOfEmptySpaceAroundCrosses.push([x-1,y]);
+        };
+      };
+
       this.set_Circle = function( _x , _y ){
         this.board[_x][_y][0] = 1;
         this.$board.find('div').eq( _x + _y * this.size).addClass('circle').removeClass('empty');
+        this.addToCircleArray(_x,_y);
+        this.setArrayOfEmptySpaceAroundCircles();
       };
       this.set_Cross = function( _x , _y ){
         this.board[_x][_y][0] = -1;
         this.$board.find('div').eq( _x + _y * this.size).addClass('cross').removeClass('empty');
+        this.addToCrossArray(_x,_y);
+        this.setArrayOfEmptySpaceAroundCrosses();
       };
       this.remove_Circle = function( _x , _y ){
         this.board[_x][_y][0] = 0;
@@ -107,17 +268,11 @@ $(document).ready(function(){
                         game.showPowerArr();
                         var x_answer = game.$board.find('.maxPower').eq(0).data('x');
                         var y_answer = game.$board.find('.maxPower').eq(0).data('y');
-                        while (time != 0) {};
+
+                        if (game.findFiveCircles() == true) console.log('KONIEC: 5 kółek!');
+
                         game.set_Cross(x_answer,y_answer);
-
-
-
-
-
-
-
-
-
+                        if (game.findFiveCrosses() == true) console.log('KONIEC: 5 krzyżyków!');
 
 
 
